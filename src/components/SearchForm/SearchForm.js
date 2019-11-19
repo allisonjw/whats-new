c01592fa04f57f797527f2a821cc0a8b9b30be4a
Allison Wagner <allisonjw01@gmail.com>
2019-11-19 14:21:10 -0700

    Add the searchform component and simple css to see on DOM

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+    constructor() {
+        super()
+        this.state = {
+            input: ""
+        }
+    }
+
+handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+}
+
+handleClick = (e) => {
+    e.preventDefault();
+    const searchText = this.state.input.toLowerCase();
+    this.props.displayNewsSearch(searchText);
+    this.setState({ input: "" });
+}
+
+  render() {
+    return (
+        <form>
+            <input 
+              type="text" 
+              placeholder="Enter a news topic" 
+              name="input"
+              value={this.state.input}
+              onChange={e => this.handleChange(e)}
+
+            />
+            <button 
+              type="submit"
+              name="submit"
+              onClick={e => this.handleClick(e)}
+            >Search
+            </button>
+        </form>
+    )
+  }
+}
+
 
 export default SearchForm;
