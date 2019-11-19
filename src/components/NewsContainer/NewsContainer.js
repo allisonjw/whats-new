c1f4740798974b51bdcc73e16078c25800d638a1
Allison Wagner <allisonjw01@gmail.com>
2019-11-18 18:47:52 -0700

    Added some styling and removed some un-needed code

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import NewsArticle from '../NewsArticle/NewsArticle';
 import './NewsContainer.css'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({ news }) => {
+    const articles = news.map(article => {
+        return (
+        <NewsArticle 
+          key={article.id}
+          image={article.img}
+          headline={article.headline}
+          description={article.description}
+          url={article.url}
+        />
+        )
+      })
+  return ( 
+    <section>
+      {articles}
+    </section>
+  )
+}
 
 export default NewsContainer;
