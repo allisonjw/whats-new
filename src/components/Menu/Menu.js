1da1e31314b7469f22ee05c580eddfa965b93827
Allison Wagner <allisonjw01@gmail.com>
2019-11-18 19:26:48 -0700

    Starts JSX for rendering on the DOM

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = () => {
+    return (
+        <nav>
+            <h1>What's New?</h1>
+            <button className='menu__btn'>Local News</button>
+            <button className='menu__btn'>Technology</button>
+            <button className='menu__btn'>Entertainment</button>
+            <button className='menu__btn'>Science</button>
+            <button className='menu__btn'>Health</button>
+        </nav>
+    )
+}
 
 export default Menu;
