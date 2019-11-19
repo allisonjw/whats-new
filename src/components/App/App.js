3d4cce4368c4bf72dec18a74443081918fa0af06
Allison Wagner <allisonjw01@gmail.com>
2019-11-19 14:21:51 -0700

    Adds method to search with form component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import health from '../../data/health';
 import science from '../../data/science';
 import technology from '../../data/technology';
 import Menu from '../Menu/Menu';
-// import SearchForm from '../SearchForm/SearchForm';
+import SearchForm from '../SearchForm/SearchForm';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import './App.css';
 
@@ -26,12 +26,17 @@ class App extends Component {
     this.setState({currentNews: this[e.target.name]})
   }
 
+  displayNewsSearch = input => {
+    const searchResults = this.state.currentNews.filter(article => article.headline.toLowerCase().includes(input));
+    this.setState({ currentNews: searchResults});
+  }
+
   render () {
     return (
       <div className="app">
         <Menu changeNewsType={this.changeNewsType}/>
         <main>
-          {/* <SearchForm /> */}
+          <SearchForm displayNewsSearch={this.displayNewsSearch}/>
           <NewsContainer news={this.state.currentNews}/>
         </main>  
       </div>
