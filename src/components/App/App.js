13b66861e5397131704c02ff304c950059225f46
Allison Wagner <allisonjw01@gmail.com>
2019-11-18 17:59:27 -0700

    Starts to set up App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
+// import Menu from '../Menu/Menu';
+// import SearchForm from '../SearchForm/SearchForm';
+// import NewsContainer from '../NewsContainer/NewsContainer';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
+    this.local = local;
+    this.entertainment = entertainment;
+    this.health = health;
+    this.science = science;
+    this.technology = technology;
     this.state = {
-      local
+      currentNews: this.local
     }
   }
 
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <h1>YOUR CODE GOES HERE!</h1>
+        {/* <Menu />
+        <SearchForm />
+        <NewsContainer /> */}
       </div>
     );
   }
