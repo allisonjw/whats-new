f803565742c3b6f6ac5c47bb2805a52540dc1a24
Allison Wagner <allisonjw01@gmail.com>
2019-11-18 18:46:52 -0700

    Adds the JSX to be rendered to the DOM

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = ({ image, headline, description, url }) => {
+  return (
+    <article>
+      <img src={ image } alt=""/>
+      <h3>{ headline }</h3>
+      <p>{ description }</p>
+    <footer>
+      <a href={ url }>
+      See full article
+      </a>
+    </footer>
+    </article>
+  )
+}
+
 
 export default NewsArticle;
