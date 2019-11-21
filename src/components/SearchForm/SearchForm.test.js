38fde9c7026702c7f35995154f4b6f9f33455a4f
Allison Wagner <allisonjw01@gmail.com>
2019-11-20 20:09:54 -0700

    Added before each hook to search form hook

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -3,11 +3,15 @@ import { shallow } from 'enzyme';
 import SearchForm from './SearchForm';
 
 describe('SearchForm', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<SearchForm
-      
-    />);
+  let wrapper;
+  const mockDisplayNewsSearch = jest.fn();
 
+  beforeEach(() => {
+    wrapper = shallow(<SearchForm displayNewsSearch={mockDisplayNewsSearch} />)
+  });
+  
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
 });
