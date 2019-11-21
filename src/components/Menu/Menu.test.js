6654a3f19e5a2dac61bead9e34e1bc2dbe969481
Allison Wagner <allisonjw01@gmail.com>
2019-11-21 14:16:07 -0700

    Adds test for each button in menu

--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -3,11 +3,35 @@ import { shallow } from 'enzyme';
 import Menu from './Menu';
 
 describe('Menu', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<Menu
-      
-    />);
+   let mockChangeNewsType = jest.fn();
+   let wrapper = shallow(<Menu changeNewsType={mockChangeNewsType} />);
 
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call the changeNewsType prop when clicked', () => {
+    wrapper.find('button').at(0).simulate('click');
+    expect(mockChangeNewsType).toHaveBeenCalled();
+  });
+
+  it('should call the changeNewsType prop when clicked', () => {
+    wrapper.find('button').at(1).simulate('click');
+    expect(mockChangeNewsType).toHaveBeenCalled();
+  });
+
+  it('should call the changeNewsType prop when clicked', () => {
+    wrapper.find('button').at(2).simulate('click');
+    expect(mockChangeNewsType).toHaveBeenCalled();
+  });
+
+  it('should call the changeNewsType prop when clicked', () => {
+    wrapper.find('button').at(3).simulate('click');
+    expect(mockChangeNewsType).toHaveBeenCalled();
+  });
+
+  it('should call the changeNewsType prop when clicked', () => {
+    wrapper.find('button').at(4).simulate('click');
+    expect(mockChangeNewsType).toHaveBeenCalled();
   });
 });
