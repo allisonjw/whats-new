63d3c0f4fa03fa1779c08659b909789f7aadb23a
Allison Wagner <allisonjw01@gmail.com>
2019-11-20 18:32:35 -0700

    Starts test for searchform

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import SearchForm from './SearchForm';
 
 describe('SearchForm', () => {
-  it.skip('should match the snapshot', () => {
+  it('should match the snapshot', () => {
     const wrapper = shallow(<SearchForm
       
     />);
