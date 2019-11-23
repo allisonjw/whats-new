ae8899d9a1811e361c83eb978611903456e86ebe
Allison Wagner <allisonjw01@gmail.com>
2019-11-22 18:54:45 -0700

    Updates

---
+++ precache-manifest.4c32ad8afa7bf9bc4091faf71894179d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815e13814196556840dea4a43c786099",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c531a0ef8371f2ab36b6",
+    "url": "./static/css/main.c17bbc5a.chunk.css"
+  },
+  {
+    "revision": "b67c0cb426729706be0a",
+    "url": "./static/js/2.29732460.chunk.js"
+  },
+  {
+    "revision": "c531a0ef8371f2ab36b6",
+    "url": "./static/js/main.349a6b5b.chunk.js"
+  },
+  {
+    "revision": "a7ba3a89543cc5257511",
+    "url": "./static/js/runtime-main.bf980d17.js"
+  }
+]);
