d5768c90921b8cfdd689534a56a67031e81d6436
Allison Wagner <allisonjw01@gmail.com>
2019-11-22 17:00:25 -0700

    Runs final linter on news article test

--- src/components/NewsArticle/NewsArticle.test.js
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -3,14 +3,14 @@ import { shallow } from 'enzyme';
 import NewsArticle from './NewsArticle';
 
 describe('NewsArticle', () => {
-    it('should match the snapshot with all data passed in correctly', () => {
-        const wrapper = shallow(<NewsArticle
-          img='https://theknow.denverpost.com/wp-content/uploads/2016/03/CDXXTHEWHODSC_4082x.jpg'
-          headline='The Who postpones Denver Concert at the Pepsi Center'
-          description='Classic-rock band The Who has postponed its Denver concert, set for Sunday, Sept. 29, after an illness robbed singer Roger Daltrey of his voice, the band said on Twitter.'
-          url='https://theknow.denverpost.com/2019/09/27/the-who-postpones-denver-concert/225182/'
-        />);
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<NewsArticle
+      img="https://theknow.denverpost.com/wp-content/uploads/2016/03/CDXXTHEWHODSC_4082x.jpg"
+      headline="The Who postpones Denver Concert at the Pepsi Center"
+      description="Classic-rock band The Who has postponed its Denver concert, set for Sunday, Sept. 29, after an illness robbed singer Roger Daltrey of his voice, the band said on Twitter."
+      url="https://theknow.denverpost.com/2019/09/27/the-who-postpones-denver-concert/225182/"
+    />);
     
-        expect(wrapper).toMatchSnapshot();
-      });
-    });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
